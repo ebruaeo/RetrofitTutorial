b607baadf1de3a5cb9f506a2cfb1bf52a03d650f
ebruaeo <ebruaeo@hotmail.com>
2024-08-19 20:06:19 +0300

    @POST and @Body annotations

--- app/src/main/java/com/example/retrofittutorial/WizardWorldAPIService.kt
+++ app/src/main/java/com/example/retrofittutorial/WizardWorldAPIService.kt
@@ -1,7 +1,10 @@
 package com.example.retrofittutorial
 
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface WizardWorldAPIService {
@@ -15,4 +18,9 @@ interface WizardWorldAPIService {
         @Query("LastName") lastName: String? = null
 
     ): List<Wizard>
+
+    @Headers("accept: text/plain", "Content-Type: application/json-patch+json")
+    @POST("/Feedback")
+    suspend fun submitFeedback(@Body feedback: Feedback): Response<Void>
+
 }
