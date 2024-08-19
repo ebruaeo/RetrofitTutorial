8825edc4ec8933e5cc47cbf475c7412c8dba6c34
ebruaeo <ebruaeo@hotmail.com>
2024-08-19 20:05:17 +0300

    submitFeedback function created

--- app/src/main/java/com/example/retrofittutorial/MyViewModel.kt
+++ app/src/main/java/com/example/retrofittutorial/MyViewModel.kt
@@ -5,10 +5,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import java.util.UUID
 
 class MyViewModel : ViewModel() {
     private val _harryPotterData = MutableLiveData("No data")
     val harryPotterData: LiveData<String> get() = _harryPotterData
+     val suggestion = Feedback(
+        "Suggestion",
+        feedback = "Perhaps rename the Wizards endpoint to smt like WizardInventors.",
+        entityId = UUID.randomUUID().toString()
+
+    )
 
     fun getHouses() {
         viewModelScope.launch {
@@ -16,11 +23,19 @@ class MyViewModel : ViewModel() {
         }
     }
 
-      fun getWizard(firstName: String? = null, lastName: String? = null) {
-          viewModelScope.launch {
-              _harryPotterData.value =
-                  RetrofitClient.wizardWorldAPIService.getWizards(firstName, lastName).toString()
-          }
+    fun getWizard(firstName: String? = null, lastName: String? = null) {
+        viewModelScope.launch {
+            _harryPotterData.value =
+                RetrofitClient.wizardWorldAPIService.getWizards(firstName, lastName).toString()
+        }
 
+    }
+
+    fun submitFeedback(feedback: Feedback) {
+        viewModelScope.launch {
+            _harryPotterData.value =
+                RetrofitClient.wizardWorldAPIService.submitFeedback(feedback).toString()
+
+        }
     }
 }
